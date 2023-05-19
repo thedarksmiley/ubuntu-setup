890913ad92a39d1584563663fe85a20c3ca7f1ba
the dark smiley <51993154+thedarksmiley@users.noreply.github.com>
2023-05-19 23:31:18 +0530

    Add files via upload

---
+++ extensions.sh
@@ -0,0 +1,15 @@
+arrangeWindows@sun.wxg@gmail.com
+blur-my-shell@aunetx
+custom-hot-corners-extended@G-dH.github.com
+dash-to-dock-cosmic-@halfmexicanhalfamazing@gmail.com
+fullscreen-hot-corner@sorrow.about.alice.pm.me
+gsconnect@andyholmes.github.io
+hot-bottom@fthx
+hotedge@jonathan.jdoda.ca
+Move_Clock@rmy.pobox.com
+rounded-window-corners@yilozt
+roundedSystemMenuButtons@pratap.fastmail.fm
+shutdownbutton@asr-webdesign.fr
+user-theme@gnome-shell-extensions.gcampax.github.com
+window-calls-extended@hseliger.eu
+window-calls@domandoman.xyz
