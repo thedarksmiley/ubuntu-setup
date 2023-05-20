fb9efe19b71e189cdf1681563ecf0a77a077826c
the dark smiley <51993154+thedarksmiley@users.noreply.github.com>
2023-05-20 12:13:27 +0530

    Update extensions.sh

--- extensions.sh
+++ extensions.sh
@@ -1,15 +1,16 @@
-arrangeWindows@sun.wxg@gmail.com
-blur-my-shell@aunetx
-custom-hot-corners-extended@G-dH.github.com
-dash-to-dock-cosmic-@halfmexicanhalfamazing@gmail.com
-fullscreen-hot-corner@sorrow.about.alice.pm.me
-gsconnect@andyholmes.github.io
-hot-bottom@fthx
-hotedge@jonathan.jdoda.ca
-Move_Clock@rmy.pobox.com
-rounded-window-corners@yilozt
-roundedSystemMenuButtons@pratap.fastmail.fm
-shutdownbutton@asr-webdesign.fr
-user-theme@gnome-shell-extensions.gcampax.github.com
-window-calls-extended@hseliger.eu
-window-calls@domandoman.xyz
+gnome-extensions-cli install arrangeWindows@sun.wxg@gmail.com
+gnome-extensions-cli install blur-my-shell@aunetx
+gnome-extensions-cli install custom-hot-corners-extended@G-dH.github.com
+gnome-extensions-cli install dash-to-dock-cosmic-@halfmexicanhalfamazing@gmail.com
+gnome-extensions-cli install fullscreen-hot-corner@sorrow.about.alice.pm.me
+gnome-extensions-cli install gsconnect@andyholmes.github.io
+gnome-extensions-cli install hot-bottom@fthx
+gnome-extensions-cli install hotedge@jonathan.jdoda.ca
+gnome-extensions-cli install Move_Clock@rmy.pobox.com
+gnome-extensions-cli install rounded-window-corners@yilozt
+gnome-extensions-cli install roundedSystemMenuButtons@pratap.fastmail.fm
+gnome-extensions-cli install shutdownbutton@asr-webdesign.fr
+gnome-extensions-cli install user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-extensions-cli install window-calls-extended@hseliger.eu
+gnome-extensions-cli install window-calls@domandoman.xyz
+ 
